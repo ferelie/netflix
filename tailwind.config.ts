aed2011ad634f230d62f3ac49a93009784beef0f
abdallah <abdallah.yasser0501@gmail.com>
2024-09-18 20:00:06 +0300

    done until sign in component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        hero: "url('/images/hero.jpg')",
+      },
     },
   },
   plugins: [],
